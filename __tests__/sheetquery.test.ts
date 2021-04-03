ad30d62fd725be94112e0245b55b647c64038bd8
Vance Lucas <vance@vancelucas.com>
2021-04-03 15:37:08 +0000

    Add test for 'where' operation

--- __tests__/sheetquery.test.ts
+++ __tests__/sheetquery.test.ts
@@ -5,20 +5,55 @@ import { Spreadsheet, Sheet } from 'gasmask/dist/SpreadsheetApp';
 // @ts-ignore
 //global.SpreadsheetApp = SpreadsheetApp;
 
+const SHEET_NAME = 'TestSheet';
 let ss = new Spreadsheet();
 
-beforeAll(() => {
-  ss.insertSheet('TestSheet');
+let sheet = new Sheet(SHEET_NAME);
+const sheetData = [
+  ['Date', 'Amount', 'Name', 'Category'],
+  ['2021-01-01', 5.32, 'Kwickiemart', 'Shops'],
+  ['2021-01-02', 72.48, 'Shopmart', 'Shops'],
+  ['2021-01-03', 1.97, 'Kwickiemart', 'Shops'],
+  ['2021-01-03', 43.87, 'Gasmart', 'Gas'],
+  ['2021-01-04', 824.93, 'Wholepaycheck', 'Groceries'],
+];
+
+beforeEach(() => {
+  if (sheet) {
+    ss.deleteSheet(sheet);
+  }
+
+  ss.insertSheet(SHEET_NAME);
+
+  sheet = ss.getSheetByName(SHEET_NAME);
+  sheetData.forEach(row => sheet.appendRow(row));
 });
 
 describe('SheetQuery', () => {
   describe('getRows', () => {
-    it('should return rows for spreadsheet', () => {
+    it('should return all rows for spreadsheet', () => {
       const query = sheetQuery(ss)
-        .from('TestSheet');
+        .from(SHEET_NAME);
       const rows = query.getRows();
 
-      expect(rows).toEqual(null);
+      expect(rows.length).toBe(5);
+      expect(rows).toContainEqual({
+        "Amount": 72.48,
+        "Category": "Shops",
+        "Date": "2021-01-02",
+        "Name": "Shopmart",
+        "__meta": {"cols": 4, "row": 3},
+      });
+    });
+
+    it('should only return rows that match Category = Shops', () => {
+      const query = sheetQuery(ss)
+        .from(SHEET_NAME)
+        .where(row => row.Category === 'Shops');
+      const rows = query.getRows();
+
+      expect(rows.length).toBe(3);
+      expect(rows.every(row => row.Category === 'Shops')).toBeTruthy();
     });
   });
 });
